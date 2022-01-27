592485512fb084f423dd3d6bc35d73f3c8e6ae7e
Ethan Woods <etwodev@protonmail.ch>
2022-01-27 15:49:31 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,6 @@ module.exports = {
     theme: {
         extend: {},
     },
-    themeConfig: {
-        default: 'cupcake'
-    },
     variants: {
         extend: {},
     },
@@ -15,85 +12,4 @@ module.exports = {
     ],
     daisyui: {
         logs: false
-    },
-    github: {
-      username: 'Etwodev', // Your GitHub org/user name. (Required)
-      sortBy: 'stars', // stars | updated
-      limit: 8, // How many projects to display.
-      exclude: {
-        forks: false, // Forked projects will not be displayed if set to true.
-        projects: [] // These projects will not be displayed. example: ['my-project1', 'my-project2']
-      }
-    },
-    skills: [
-      'Python',
-      'php',
-      'Dart',
-      'CSS',
-      'JavaScript'
-    ],
-    experiences: [
-      {
-        company: 'Fireplace',
-        position: 'Backend Software Engineer',
-        from: 'January 2022',
-        to: 'Present'
-      }
-    ],
-    education: [
-      {
-        institution: 'King Edwards VI',
-        degree: 'Computer Science A-Level',
-        from: '2021',
-        to: 'Present'
-      },
-      {
-        institution: 'King Edwards VI',
-        degree: 'Maths A-Level',
-        from: '2021',
-        to: 'Present'
-      },
-      {
-        institution: 'King Edwards VI',
-        degree: 'Physics A-Level',
-        from: '2021',
-        to: 'Present'
-      },
-      {
-        institution: 'King Edwards VI',
-        degree: 'A* Computer Science GCSE',
-        from: '2016',
-        to: '2021'
-      },
-      {
-        institution: 'King Edwards VI',
-        degree: 'A* English Language GCSE',
-        from: '2016',
-        to: '2021'
-      },
-      {
-        institution: 'King Edwards VI',
-        degree: '9 Maths GCSE',
-        from: '2016',
-        to: '2021'
-      },
-      {
-        institution: 'King Edwards VI',
-        degree: '8 Physics GCSE',
-        from: '2016',
-        to: '2021'
-      },
-      {
-        institution: 'King Edwards VI',
-        degree: '7 Biology GCSE',
-        from: '2016',
-        to: '2021'
-      },
-      {
-        institution: 'King Edwards VI',
-        degree: '7 Chemistry GCSE',
-        from: '2015',
-        to: '2019'
-      }
-    ]
-}
+    }
