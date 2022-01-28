28f89c8ecba7eb395b7406a4d02632faca13cb2a
Etwodev <etwodev@protonmail.ch>
2022-01-28 22:41:18 +0000

    404 page added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import PageNotFound from './PageNotFound.tsx';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LoadingProvider } from './contexts/LoadingContext';
 import { setupHotjar } from './helpers/utils';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 ReactDOM.render(
+    <Router>
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    </Router>,
+    document.getElementById('root')
+);
+
+function NotFound () {
+    return (
+        <a href="/">
+            <PageNotFound/>
+        </a>
+    );
+}
+
+function Homepage () {
+    return (
     <React.StrictMode>
         <ThemeProvider>
             <LoadingProvider>
@@ -17,9 +38,10 @@ ReactDOM.render(
                 </HelmetProvider>
             </LoadingProvider>
         </ThemeProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+    </React.StrictMode>
+    );
+}
+
 
 reportWebVitals();
 setupHotjar();
